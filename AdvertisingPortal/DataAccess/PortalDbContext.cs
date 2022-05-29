78d16e7119b879d28035c44a7e02cbd29b334cb5
JulianKocon <s23749@pjwstk.edu.pl>
2022-05-29 15:31:06 +0200

    Added Migration DataSeed

--- AdvertisingPortal/DataAccess/PortalDbContext.cs
+++ AdvertisingPortal/DataAccess/PortalDbContext.cs
@@ -23,7 +23,76 @@ namespace AdvertisingPortal.DataAccess
             modelBuilder.Entity<PurchaseOrder>()
                 .HasKey(c => new { c.IdUser, c.IdAdvertisement });
 
-          
+            modelBuilder.Entity<Region>(a =>
+            {
+                a.HasData(
+                    new Region { IdRegion = 1, Name = "Warsaw, Poland" }
+                    );
+
+                a.HasData(
+                    new Region { IdRegion = 2, Name = "Gdynia, Poland" }
+                    );
+
+                a.HasData(
+                    new Region { IdRegion = 3, Name = "New York, USA" }
+                    );
+
+                a.HasData(
+                    new Region { IdRegion = 4, Name = "Dallas, USA" }
+                    );
+
+            });
+
+            modelBuilder.Entity<Category>(a =>
+            {
+                
+
+                a.HasData(
+                    new Category { IdCategory = 1, Name = "Vehicles" }
+                    );
+
+                a.HasData(
+                    new Category { IdCategory = 2, Name = "Real estate" }
+                    );
+
+                a.HasData(
+                   new Category { IdCategory = 3, Name = "House and garden" }
+                   );
+
+                a.HasData(
+                   new Category { IdCategory = 4, Name = "Pets" }
+                   );
+            });
+
+            modelBuilder.Entity<Advertisement>(a =>
+            {
+                a.HasData(
+                    new Advertisement
+                    {
+                        IdAdvertisement = 1,
+                        IdUser = 1,
+                        Name = "Toyota mr2",
+                        Description = "Old but gold",
+                        Date = DateTime.Parse("18/05/2022"),
+                        IdRegion = 1,
+                        Price = 24000
+                    }
+                    );
+
+                a.HasData(
+                   new Advertisement
+                   {
+                       IdAdvertisement = 2,
+                       IdUser = 1,
+                       Name = "Pets",
+                       Description = "White Maine Coon for sale",
+                       Date = DateTime.Parse("18/05/2022"),
+                       IdRegion = 1,
+                       Price = 1000
+                   }
+                   );
+            });
+
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Advertisement> Advertisements { get; set; }
---
+++ AdvertisingPortal/Migrations/20220529132936_DataSeed.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdvertisingPortal.Migrations
+{
+    public partial class DataSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Advertisements",
+                columns: new[] { "IdAdvertisement", "Date", "Description", "IdRegion", "IdUser", "Name", "Price", "Region", "RegionIdRegion" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2022, 5, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "Old but gold", 1, 1, "Toyota mr2", 24000m, null, null },
+                    { 2, new DateTime(2022, 5, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "White Maine Coon for sale", 1, 1, "Pets", 1000m, null, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "IdCategory", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Vehicles" },
+                    { 2, "Real estate" },
+                    { 3, "House and garden" },
+                    { 4, "Pets" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "IdRegion", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Warsaw, Poland" },
+                    { 2, "Gdynia, Poland" },
+                    { 3, "New York, USA" },
+                    { 4, "Dallas, USA" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Advertisements",
+                keyColumn: "IdAdvertisement",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Advertisements",
+                keyColumn: "IdAdvertisement",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "IdCategory",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "IdCategory",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "IdCategory",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "IdCategory",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "IdRegion",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "IdRegion",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "IdRegion",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "IdRegion",
+                keyValue: 4);
+        }
+    }
+}
